0f20d4466a95f48d5731f991c58b86f085d68135
GOVIND JHA <govindjha@GOVINDs-MacBook-Air.local>
2024-08-18 20:47:26 +0530

    new

--- src/app/commons/components/elements/copyright.js
+++ src/app/commons/components/elements/copyright.js
@@ -1,9 +1,16 @@
+import React from "react";
+
 export default function Copyright() {
   return (
-    <div className="text-neutral-700 text-sm   px-3">
+    <div className="text-neutral-600 text-sm px-3">
       <div className="animate-pulse cursor-pointer">
         <span>jhagovind348@gmail.com</span>
       </div>
+      <img
+        src="/signature.png"
+        alt="Signature"
+        className="hidden md:block md:w-[800px] md:h-[60px] filter brightness-50" // Show only on medium and larger screens
+      />
     </div>
   );
 }
